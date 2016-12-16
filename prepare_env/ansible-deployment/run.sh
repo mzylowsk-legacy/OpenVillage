98d41be3318efca272c02588482744a361e2abee
endrius <joskowski.andrzej@gmail.com>
2016-12-16 21:04:00 +0100

    Ansible script improvements
    Signed-off-by: endrius <joskowski.andrzej@gmail.com>

--- prepare_env/ansible-deployment/run.sh
+++ prepare_env/ansible-deployment/run.sh
@@ -6,9 +6,6 @@
 # For running script => ./run.sh -u <username> -k --ask-sudo-pass
 #
 
-export ANSIBLE_SSH_CONTROL_PATH='%(directory)s/%%h-%%r'
-export ANSIBLE_HOST_KEY_CHECKING=False
-
 ansible-playbook site.yml -i inventory.yml $@
 
 RET_CODE=$?
