768cb3bf795dd11847bb29ae5f53f5899f4b9a47
Marcin K <mmmanio93@gmail.com>
2017-07-26 08:31:25 +0200

    PG-61: JSHint script and configuration added

---
+++ back-end/server/assets/default_scripts/jshint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+FILES=`find . -name "*.js"`
+
+if ["$FILES" = "" ]; then
+  exit 0
+fi
+
+pass=true
+for file in ${FILES}; do
+  echo "Validating ${file} file..."
+  result=$(jshint ${file})
+  if ["$result" != "" ]; then
+    echo "$result"
+    pass=false
+  fi
+done
+
+if ! $pass; then
+  exit 1
+else
+  exit 0
+fi
