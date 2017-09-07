d250a67d424b28acb0c5227ed7d3f3df4d3654cf
Marcin K <mmmanio93@gmail.com>
2017-09-07 19:30:29 +0200

    PG-62: Restore config

---
+++ back-end/server/config/config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports.loggerConf = {
+    filePath: '/tmp/ProjectsBuilderPlatform.log',
+    level: 'debug'
+};
+
+module.exports.emailProvider = {
+    host: '<host>',
+    from: 'OpenVillage <email>',
+    port: 465,
+    secure: true,
+    username: '<username>',
+    password: '<password>'
+};
+
+module.exports.auth = {
+    expirationTokenTime: '5h',
+    key: '<key>'
+};
+
+module.exports.environment = {
+    host: '<protocol>:<address>:<port>'
+};
+
+module.exports.mongodb = {
+    host: 'mongodb://localhost',
+    port: 27017,
+    databaseName: 'openvillage'
+};
+
+module.exports.builder = {
+    workspace: { path: '/tmp/openvillage' }
+};
